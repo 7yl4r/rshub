4e88390d8ebe2e525b874bb5dfb8e1eceec48827
Tylar <email@tylar.info>
2025-02-25 13:31:55 -0500

    Create setup_01.sh

---
+++ setup_01.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y python3 python3-pip python3-dev nodejs npm curl
+
+sudo apt install -y build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev
+
+sudo pip3 install jupyterhub jupyterlab
+
+sudo npm install -g configurable-http-proxy
+
+sudo pip3 install -r requirements.txt
